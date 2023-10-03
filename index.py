aa5f0fcb867f242ba1fd5da2bf9cf2f8295cae66
peteraszxdc <peteraszxdc@gmal.com>
2023-10-03 14:14:26 +0800

    m

--- index.py
+++ index.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
 
 
 @app.route("/hellow_world")
